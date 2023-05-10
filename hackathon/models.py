31a725b61f4ba370bc8c2a0a1c63eeb3f8563202
Tolu Joel <toluisjoel@gmail.com>
2023-05-10 13:59:25 +0100

    Added Hackathon model

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Hackathon(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    background_image = models.ImageField(upload_to='hackathon_backgrounds/')
+    hackathon_image = models.ImageField(upload_to='hackathon_images/')
+    SUBMISSION_TYPE_CHOICES = [
+        ('image', 'Image'),
+        ('file', 'File'),
+        ('link', 'Link'),
+    ]
+    sumbission_type = models.CharField(max_length=10, choices=SUBMISSION_TYPE_CHOICES)
+    end_datetime = models.DateTimeField()
+    reward_prize = models.PositiveIntegerField()
+    start_datetime = models.DateTimeField()
+    
+    organizer = models.ManyToManyField(get_user_model(), related_name='organized_hackathons')
+
+    def __str__(self):
+        return self.title
+    
