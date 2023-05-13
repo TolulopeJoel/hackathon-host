b77eb3b8b09a1c598744b35dd9e414f14db717c4
Tolu Joel <toluisjoel@gmail.com>
2023-05-13 06:03:52 +0100

    Added serializer and endpoint to register user

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from . import views
+
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
