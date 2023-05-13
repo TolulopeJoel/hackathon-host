79caa575f3bc70d9acd847190c4d0458bcbec1a9
Tolu Joel <toluisjoel@gmail.com>
2023-05-13 06:54:49 +0100

    Passed hackaton to submission serailizer using perform_create method

--- submissions/views.py
+++ submissions/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import NotFound
+
+from hackathon.models import Hackathon
 
 from .models import Submission
 from .serializers import SubmissionSerializer
@@ -14,4 +17,11 @@ class SubmissionsViewset(viewsets.ModelViewSet):
         
         return queryset.filter(user=user)
 
+    def perform_create(self, serializer):
+        hackathon_id = self.request.data.get('hackathon_id')
+        try:
+            hackathon = Hackathon.objects.get(pk=hackathon_id)
+        except Hackathon.DoesNotExist:
+            raise NotFound('Hackathon not found')
+        serializer.save(user=self.request.user, hackathon=hackathon)
 
